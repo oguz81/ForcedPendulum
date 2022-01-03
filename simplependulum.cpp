3f4834c602d95cffa2762fc48a75a5c594c7cbf0
oguz81 <ouzdemirtas@hotmail.com>
2022-01-03 13:08:27 +0300

    TEST

--- simplependulum.cpp
+++ simplependulum.cpp
@@ -9,7 +9,27 @@
 #include <glm/gtc/type_ptr.hpp>
 GLFWwindow* window;
 
+#define PI 3.141592
+#define h 0.01
 //denemeler
+#define k 0.3 
+#define THETA_0 0.5       //initial angle
+#define omega_0 0     //initial angular velocity
+
+#define A 2.4
+#define b 0
+#define m 1
+#define R 1
+#define grav 1
+
+
+float f(float time, float tht,float omega){
+        return omega;
+}
+float g(float time, float tht, float omega){
+        return -(grav/R)*sin(PI*tht)-((b/(m*R*R))*omega)+((A/(m*R*R))*cos(PI*k*time));
+}
+
 
 int main( void )
 {
@@ -101,12 +121,37 @@ int main( void )
     //position attribute
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);   
+
+    float theta= THETA_0;
+        float omg= omega_0;
+        float k1,k2,k3,k4,l1,l2,l3,l4;
+        float time = 0;
+
+        f(time,theta,omg);
+        g(time,theta,omg);
+        float ada;
     do{
+
+        k1= h*f(time,theta,omg);
+                l1= h*g(time,theta,omg);
+                k2= h*f(time+(0.5*h),theta+(0.5*k1),omg+(0.5*l1));
+                l2= h*g(time+(0.5*h),theta+(0.5*k1),omg+(0.5*l1));
+                k3= h*f(time+(0.5*h),theta+(0.5*k2),omg+(0.5*l2));
+                l3= h*g(time+(0.5*h),theta+(0.5*k2),omg+(0.5*l2));
+                k4= h*f(time+h,theta+k3,omg+l3);
+                l4= h*g(time+h,theta+k3,omg+l3);
+
+                theta = theta+(k1 + (2*k2) + (2*k3) + k4)/6;
+                omg = omg+(l1 + (2*l2) + (2*l3) + l4)/6;
+        //if(theta>1) theta= theta-2;
+        //if(theta<-1) theta= theta+2;
+        ada = theta * 180 / PI;
+        time =time+h;
         float timem = glfwGetTime() * 1;
-        std::cout<<timem <<std::endl;
+        std::cout<<ada<<std::endl;
         glClear( GL_COLOR_BUFFER_BIT );
         glUseProgram(shaderProgram);
-        float ada= sin(timem) * 100;
+        
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
